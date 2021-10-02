98a69b05648c21ef2ddfa59a107635db8c4f45c2
kimyounghoon00 <kim_younghoon@outlook.com>
2021-10-02 23:05:19 +0900

    second commit

--- app.py
+++ app.py
@@ -1,1 +1,102 @@
-import bs4
+import requests
+from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#
+# headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data = requests.get('https://www.youtube.com/channel/UCtwxuughKbSAV0e4bnz4ORg/videos',headers=headers)
+# #
+# soup = BeautifulSoup(data.text, 'html.parser')
+#
+#
+# # 코딩 시작
+
+driver = webdriver.Chrome()
+url = 'https://www.youtube.com/channel/UCtwxuughKbSAV0e4bnz4ORg/videos'
+driver.get(url)
+page=driver.page_source
+
+
+movies=driver.find_elements_by_id("details") # 영화 내용 및 조회수
+
+
+# for movie in movies:
+#     print(movie.text)
+
+
+# for i in range(len(movies)): # 타이틀 리스트 만듬 계속 조회수랑 업데이트 딸려옴 ㅠㅠ -> 알고보니 같은 인덱스에 전부 포함. 각각 크롤링 해야할 듯...타이틀, 조회수, 업데이트 날짜...
+#     title = []
+#     title.append(movies[i].text)
+#     print(i,title)
+
+# play_time=driver.find_elements_by_id("thumbnail") # 재생 시간 근데 리스트 값에 공란 존재함 어떻게 공란은 제거하는지
+# for time in play_time:
+#     if time is not None:
+#         print(time.text)
+
+#
+# for i in range(len(play_time)): #재생 시간 리스트 만듬 [''] 제거 예정. 반복문 돌려야 하나?
+#     video_time=[]
+#
+#     video_time.append(play_time[i].text)
+#
+#     print(i, video_time)
+
+
+
+
+# for i in range(len(play_time)): #재생 시간 리스트 만듬 [''] 제거 예정. 반복문 돌려야 하나? remove 함수 사용???
+#     video_time=[]
+#
+#     video_time.append(play_time[i].text)
+#
+# while '['']' in video_time:
+#     video_time.remove('['']')
+
+
+
+# hong_contents=driver.find_elements_by_id("contents") # 영화 제목 조회수 및 재생 시간...근데 전부 하나에
+# for content in hong_contents:
+#     print(content.text)
+
+# img_path="//*[@id="thumbnail"]/yt-img-shadow"
+# //*[@id="dismissible"]/ytd-thumbnail
+# //*[@id="thumbnail"]
+
+
+
+
+#이미지 썸네일
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]
+#
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]/div[1]/ytd-thumbnail
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]/div[1]/ytd-thumbnail/a
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]/div[1]/ytd-thumbnail/a/yt-img-shadow
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]/div[1]/ytd-thumbnail/a/yt-img-shadow/img
+#
+# #재생시간
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]/div[1]/ytd-thumbnail/a/div[1]/ytd-thumbnail-overlay-time-status-renderer
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]/div[1]/ytd-thumbnail/a/div[1]/ytd-thumbnail-overlay-time-status-renderer/span
+#
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[4]/div[1]/ytd-thumbnail/a/div[1]/ytd-thumbnail-overlay-time-status-renderer/span
+#
+# #조회수 리스트
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[4]/div[1]/div[1]/div[1]/div/div[1]/div[2]/span[1]
+# /html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[3]/div[1]/div[1]/div[1]/div/div[1]/div[2]/span[1]
+#
+#
+# #링크
+/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer['+ str(idx) + ']/div[1]/div[1]/div[1]/h3/a
+
+
+
+
+
+
+
+
+
+
---
+++ ilnam.py
@@ -0,0 +1,95 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def get_image_title(url):
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)  # or bigger second
+
+    # 열고자 하는 채널 -> 동영상 목록으로 된 url 페이지
+    driver.get(url)
+
+    image_list = list()  # 썸네일을 받을 수 있는 주소 저장용 리스트
+    title_list = list() # 제목
+    play_time_list=list() # 썸네일 재생시간을 저장하는 리스트 가장 중요!!!
+    views_list=list() # 조회수
+    video_list = list() # 비디오 url 저장용 라스트
+
+
+    idx = 1
+    while True:
+        try:
+            img_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[' + str(
+                idx) + ']/div[1]/ytd-thumbnail/a/yt-img-shadow/img'
+            title_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[' + str(
+                idx) + ']/div[1]/div[1]/div[1]/h3/a'
+            play_time_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[' + str(
+                idx) + ']/div[1]/ytd-thumbnail/a/div[1]/ytd-thumbnail-overlay-time-status-renderer/span'
+            views_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer['+ str(
+                idx) + ']/div[1]/div[1]/div[1]/div/div[1]/div[2]/span[1]'
+            video_link_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer['+ str(
+                idx) + ']/div[1]/div[1]/div[1]/h3/a'
+
+
+
+            # 이미지가 곧바로 로드 되지 않을 때, 20초간 강제로 기다림
+            img = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, img_xpath)))
+            if img is None:
+                print(idx, 'img is not loaded.')
+
+            # 한 페이지에 약 8개 불러오는 데, 동영상 목록을 추가 불러오기 위해 스크롤 내림
+            if idx % 8 == 0:
+                driver.execute_script('window.scrollBy(0, 1080);')
+                time.sleep(2)
+                driver.execute_script('window.scrollBy(0, 1080);')
+                time.sleep(2)
+                driver.execute_script('window.scrollBy(0, 1080);')
+                time.sleep(2)
+
+            # 썸네일 주소를 리스트에 저장
+            image = driver.find_element_by_xpath(img_xpath)
+            img_url = image.get_attribute('src')
+            image_list.append(img_url)
+
+            # 타이틀을 리스트에 저장
+            title = driver.find_element_by_xpath(title_xpath)
+            title_list.append(title.text)
+
+            #재생시간 리스트에 저당
+            play_time = driver.find_element_by_xpath(play_time_xpath)
+            play_time_list.append(play_time.text)
+
+            #조회수 리스트에 저장
+            view_time = driver.find_element_by_xpath(views_xpath)
+            views_list.append(view_time.text)
+
+
+            #동영상 링크 리스트에 저장
+            video = driver.find_element_by_xpath(video_link_xpath)
+            video_url = video.get_attribute('href')
+            video_list.append(video_url)
+
+            print(idx, title.text, img_url, play_time.text, view_time.text, video_url)
+
+            idx += 1
+        except Exception as e:
+            print()
+            print(e)
+            break
+    assert len(image_list) == len(title_list)
+    driver.close()
+    return image_list, title_list, play_time_list, views_list, video_list
+
+
+# 진솔한 리뷰
+url1 = 'https://www.youtube.com/channel/UC3DNe5b3NYZ5ojre8YE1_xw/videos'
+image1, title1, play_time1, view_time1, video1 = get_image_title(url1)
+
+
+
+
